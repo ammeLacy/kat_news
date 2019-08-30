e4ead0d97a714a74b5f179e00f51e094abea110f
ammelacy <emma_lacy@hotmail.com>
2019-08-30 09:18:53 +0100

    resolved errors with exports in api and missing close in index html

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,41 +2,41 @@ const axios = require('axios');
 
 const baseUrl = "https://kat-news.herokuapp.com/api";
 
-exports.getArticles = async (search) => {
+export const getArticles = async (search) => {
   const { data } = await axios.get(`${baseUrl}/articles`, { params: search });
   return data.articles;
 }
 
-exports.getArticle = async (article_id) => {
+export const getArticle = async (article_id) => {
   const { data } = await axios.get(`${baseUrl}/articles/${article_id}`)
   return data.article;
 }
 
-exports.getArticleComments = async (article_id) => {
+export const getArticleComments = async (article_id) => {
   const { data } = await axios.get(`${baseUrl}/articles/${article_id}/comments`)
   return data.comments;
 }
 
-exports.updateArticleVotes = async (article_id, vote) => {
+export const updateArticleVotes = async (article_id, vote) => {
   const { data } = await axios.patch(`${baseUrl}/articles/${article_id}`, { inc_votes: vote })
   return data.article;
 }
 
-exports.postArticleComments = async (newComment, currentUser, article_id) => {
+export const postArticleComments = async (newComment, currentUser, article_id) => {
   const { data } = await axios.post(`${baseUrl}/articles/${article_id}/comments`, { body: newComment, username: currentUser })
   return data.comment;
 }
 
-exports.deleteArticleComment = async (comment_id) => {
+export const deleteArticleComment = async (comment_id) => {
   axios.delete(`${baseUrl}/comments/${comment_id}`);
 }
 
-exports.updateCommentVotes = async (comment_id, vote) => {
+export const updateCommentVotes = async (comment_id, vote) => {
   const { data } = await axios.patch(`${baseUrl}/comments/${comment_id}`, { inc_votes: vote })
   return data.comment;
 }
 
-exports.getTopics = async () => {
+export const getTopics = async () => {
   const { data } = await axios.get(`${baseUrl}/topics`);
   return data.topics;
 }
