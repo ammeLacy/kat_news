3a978004a6eca47e353475ff4c88167d19bd00fd
ammelacy <emma_lacy@hotmail.com>
2019-09-02 11:56:23 +0100

    created loading spinner

--- src/components/multiUseComponents/LoadingSpinner.js
+++ src/components/multiUseComponents/LoadingSpinner.js
@@ -3,6 +3,7 @@ import React from 'react';
 const LoadingSpinner = () => {
   return (<>
     <p> ... Loading</p>
+    <img src="public/Logo.gif" alt="Cat spinning round" height="42" width="42"></img>
   </>);
 }
 
